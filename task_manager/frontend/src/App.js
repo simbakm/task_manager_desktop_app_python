c8676852b83c7a8f2b6f59a4b6aca277ed121ad9
simbakm <simbarashesandi@gmail.com>
2024-09-05 17:25:05 +0200

    having the react app completely created

--- task_manager/frontend/src/App.js
+++ task_manager/frontend/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TaskList from './TaskList'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <TaskList />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +21,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
