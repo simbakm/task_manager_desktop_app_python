5b1356c1def59c13d24b213166defc06af0483f4
simbakm <simbarashesandi@gmail.com>
2024-09-10 09:06:11 +0200

    start over again

--- task_manager/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-# tasks/urls.py
-from django.urls import path
-from .views import TaskList, TaskDetail
-
-urlpatterns = [
-    path('tasks/', TaskList.as_view(), name='task-list'),
-    path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-]
